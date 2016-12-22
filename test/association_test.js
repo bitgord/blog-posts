5455d99a6204a74947e5d6aea4ad69d69c1425dd
Michael Gord <michael.l.gord@gmail.com>
2016-12-21 23:04:50 -0500

    save blogpost in db

--- test/association_test.js
+++ test/association_test.js
@@ -10,9 +10,19 @@ describe('Associations', () => {
 		blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
 		comment = new Comment({ content: 'Great post' });
 
-		joe.blogPosts.posh(blogPost);
+		joe.blogPosts.push(blogPost);
 		blogPost.comments.push(comment);
 		comment.user = joe;
-		
+
+		Promise.all([joe.save(), blogPost.save(), comment.save()])
+			.then(() => done());
 	});
+
+	it.only('saves a relation between a user and a blogpost', (done) => {
+		User.findOne({ name: 'Joe' })
+			.then((user) => {
+				console.log(user);
+				done();
+			});
+	})
 });
